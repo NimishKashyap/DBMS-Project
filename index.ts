597b8fa71a4f293afbfa2c32183ab16a3f0c7c29
NimishKashyap <kashap.nimish8@gmail.com>
2021-09-21 21:40:46 +0530

    comments added

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import mysql from "mysql";
 
+// App initialization
 const app = express();
 
 
+// Follow this tutorial: https://www.youtube.com/watch?v=W8jySpfRUDY
 const database = mysql.createConnection({
     user:"root",
     host:"localhost",
@@ -11,12 +13,14 @@ const database = mysql.createConnection({
     database:"testdb"
 });
 
-
+// Initial Route
 app.get("/", (req,res)=>{
     console.log("HIT API ENDPOINT");
     res.send("Hello");
     
 })
+
+// Insert Route
 app.get("/insert",(req,res)=>{
     database.query("INSERT INTO countries (name, population) VALUES (?,?)",['bulgaria',122],(err,result)=>{
         if(err){
@@ -26,6 +30,8 @@ app.get("/insert",(req,res)=>{
         res.send(result);
     })
 })
+
+// Listen Route
 app.listen(3005, ()=>{
     console.log("Server is up and running HOOR234AY");
     
